24fadf9c2d1362b881eee9a4abe9fbd4460cf698
Seyed Ehsan Setarehdan <Eh3an@Setarehdan.ir>
2018-11-15 03:13:58 +0330

    Add files via upload

--- dlefa/upload/engine/classes/min/groupsConfig.php
+++ dlefa/upload/engine/classes/min/groupsConfig.php
@@ -1,1 +1,25 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
 
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ **/
+
+return array(
+    // custom source example
+    'dlefa' => array(
+     	$min_documentRoot . '/engine/skins/javascripts/application_fa.js', 
+    ),
+
+    'general' => array(
+     	$min_documentRoot . '/engine/classes/js/jquery.js',
+    ),
+
+    'admin' => array(
+     	$min_documentRoot . '/engine/skins/javascripts/application.js', 
+    ),
+);
